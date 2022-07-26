66d6683a66c183a54ae8b6f876738dfd06b9ae2b
Kierandoolan <kierandoolan91@gmail.com>
2022-07-26 16:59:10 +0000

    add data-name to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 const cards = document.querySelectorAll('.card')
 
+let turnCard = false;
+let firstCard, secondCard;
+
 function flipCard () {
     this.classList.add('flip')
+
+    if (!turnCard) {
+        turnCard = true ;
+        firstCard = this; 
+}   else {
+    turnCard = false ; 
+    secondCard= this;
+
+    console.log ({firstCard, secondCard})
+}
 }
     cards. forEach(card => card.addEventListener('click', flipCard))
