7e29cee68e4700360bba3b10ea676587238ae2d6
Kierandoolan <kierandoolan91@gmail.com>
2022-07-27 16:34:36 +0000

    add doubleClick function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ let firstCard, secondCard;
 
 function flipCard () {
     if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip')
 
     if (!turnCard) {
@@ -31,20 +32,27 @@ function flipCard () {
     function disableCards () {
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard);
+
+        
     } 
     
     function unflipCards () {
         lockBoard = true;
 
         setTimeout(() =>{
+            
             firstCard.classList.remove('flip');
             secondCard.classList.remove('flip');
 
-            lockBoard = false;
+
+            doubleClick ()
     }, 1000);
     }
     
-
+    function doubleClick (){
+    [turnCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    }
 
 
     cards. forEach(card => card.addEventListener('click', flipCard)) 
